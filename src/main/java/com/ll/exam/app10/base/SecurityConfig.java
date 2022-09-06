5b50152620771ccd565023775e352aa5e20a7dff
SangHun <shzero211@naver.com>
2022-09-06 15:28:16 +0900

    feat:csrf 끄기

--- src/main/java/com/ll/exam/app10/base/SecurityConfig.java
+++ src/main/java/com/ll/exam/app10/base/SecurityConfig.java
@@ -1,17 +1,25 @@
 package com.ll.exam.app10.base;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
-        http.authorizeRequests().antMatchers("/**").permitAll().and().csrf().disable();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
         return http.build();
     }
 }
