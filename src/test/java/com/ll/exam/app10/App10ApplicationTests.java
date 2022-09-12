900bdf620fb2b6d188590843280440af9ed9b265
SangHun <shzero211@naver.com>
2022-09-13 02:01:50 +0900

    refactor:mvc 로 home test

--- src/test/java/com/ll/exam/app10/App10ApplicationTests.java
+++ src/test/java/com/ll/exam/app10/App10ApplicationTests.java
@@ -1,11 +1,37 @@
 package com.ll.exam.app10;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 class App10ApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	@DisplayName("메인화면에서는 안녕이 나와야한다")
+	void t1() throws Exception {
+		ResultActions resultActions= mvc.perform(get("/")).andDo(print());
+		resultActions.andExpect(status().is2xxSuccessful())
+				.andExpect(handler().handlerType(HomeController.class))
+				.andExpect(handler().methodName("main"))
+				.andExpect(content().string(containsString("안녕")));
+	}
 	@Test
 	void contextLoads() {
 	}
