b26a43ed6aad77baca5d75de7470c6ac39afdc70
SangHun <shzero211@naver.com>
2022-09-15 09:55:07 +0900

    feat:로그인 정보 출력

--- src/main/java/com/ll/exam/app10/HomeController.java
+++ src/main/java/com/ll/exam/app10/HomeController.java
@@ -1,12 +1,15 @@
 package com.ll.exam.app10;
 
 import com.ll.exam.app10.member.Member;
+import com.ll.exam.app10.member.MemberContext;
 import com.ll.exam.app10.member.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.security.Principal;
@@ -24,5 +27,16 @@ public class HomeController {
     public String showAbout(Principal principal,Model model){
 
         return "home/about";
+    }
+    @GetMapping("/currentUserOrigin")
+    @ResponseBody
+    public Principal currentUserOrigin(Principal principal) {
+        return principal;
+    }
+
+    @GetMapping("/currentUser")
+    @ResponseBody
+    public MemberContext currentUser(@AuthenticationPrincipal MemberContext memberContext) {
+        return memberContext;
     }
 }
