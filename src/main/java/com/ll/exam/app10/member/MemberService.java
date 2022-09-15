cc8e64d41288aaae9397ccd1ad1902ea4e3fe88f
SangHun <shzero211@naver.com>
2022-09-15 09:51:57 +0900

    feat:User 객체 대신 MemberContext 사용하여 로그인 정보 세션에 저장

--- src/main/java/com/ll/exam/app10/member/MemberService.java
+++ src/main/java/com/ll/exam/app10/member/MemberService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService implements UserDetailsService {
+public class MemberService {
     @Value("${custom.genFileDirPath}")
     private String genFileDirPath;
 
@@ -72,13 +72,6 @@ public class MemberService implements UserDetailsService {
         return memberRepository.findById(id).orElse(null);
     }
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member=memberRepository.findByUsername(username);
-        List<GrantedAuthority> authorities=new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("member"));
-        return new User(member.getUsername(),member.getPassword(),authorities);
-    }
     private String getCurrentProfileImgDirName() {
         return "member/" + Util.date.getCurrentDateFormatted("yyyy_MM_dd");
     }
